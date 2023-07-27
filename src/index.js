0fa6e2c1003c5ef2dcbf774f9d8a1aaf3879d822
laosarah <139395413+laosarah@users.noreply.github.com>
2023-07-27 20:32:30 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import validator from './validator.js';
+console.log("carregou arquivo"); 
 
-console.log(validator);
+const botao = document.getElementById("meuBotao"); //pega o botão
+botao.addEventListener("click", function (event) { //clica no botao 
+    event.preventDefault(); 
+    console.log("clicou botao");
+    exibirDigito();
+
+
+})
+
+function exibirDigito() {
+    console.log("chamou a funcao exibir digito"); 
+
+    /*const inputElemento = document.getElementById("meuInput"); 
+    const userNum = inputElemento.value; 
+    const resultadoElemento = document.getElementById("resultado");
+
+    resultadoElemento.textContent = userNum;*/ 
+}
+
+
+
+
+/* CODIGO JOSI 
+const userNum = document.getElementById("userNum"); //pega o input 
+const botao = document.getElementById("meuBotao"); //pega o botão
+
+botao.addEventListener("click", function (event) {
+    event.preventDefault(); 
+    if (userNum.value === ""){
+        return document.getElementById("resultado").innerHTML = "Digite o número do cartão";
+    }
+    const vlr = userNum.value; //pega o botao 
+    //const maskedNumbers = validator.maskify(vlr); //guarda os números mascarados
+    if (validator.isValid(vlr)) { //funçao de validar 
+        return document.getElementById("resultado").innerHTML = maskedNumbers + " é um cartão válido.";
+    }
+    return document.getElementById("resultado").innerHTML = "Esse cartão não é válido.";
+})
+
+botao.addEventListener("click", function zerarInput(){
+    document.getElementById("userNum").value = ""; 
+})
+*/
