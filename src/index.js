bbe89e7ebd7ec8c0330d57bb3138ceaf229e6321
laosarah <139395413+laosarah@users.noreply.github.com>
2023-08-02 18:54:52 -0300

    update commit

--- src/index.js
+++ src/index.js
@@ -1,45 +1,39 @@
-console.log("carregou arquivo"); 
+import validator from './validator.js';
 
-const botao = document.getElementById("meuBotao"); //pega o botão
-botao.addEventListener("click", function (event) { //clica no botao 
-    event.preventDefault(); 
-    console.log("clicou botao");
-    exibirDigito();
-
-
-})
-
-function exibirDigito() {
-    console.log("chamou a funcao exibir digito"); 
-
-    /*const inputElemento = document.getElementById("meuInput"); 
-    const userNum = inputElemento.value; 
-    const resultadoElemento = document.getElementById("resultado");
-
-    resultadoElemento.textContent = userNum;*/ 
-}
-
-
-
-
-/* CODIGO JOSI 
-const userNum = document.getElementById("userNum"); //pega o input 
-const botao = document.getElementById("meuBotao"); //pega o botão
+console.log("carregou arquivo")
+const numInput = document.getElementById("meuInput"); //variavel numImput pega o elemento input 
+const botao = document.getElementById("meuBotao"); //variavel botao pega o elemento botão
 
 botao.addEventListener("click", function (event) {
-    event.preventDefault(); 
-    if (userNum.value === ""){
-        return document.getElementById("resultado").innerHTML = "Digite o número do cartão";
-    }
-    const vlr = userNum.value; //pega o botao 
-    //const maskedNumbers = validator.maskify(vlr); //guarda os números mascarados
-    if (validator.isValid(vlr)) { //funçao de validar 
-        return document.getElementById("resultado").innerHTML = maskedNumbers + " é um cartão válido.";
-    }
-    return document.getElementById("resultado").innerHTML = "Esse cartão não é válido.";
+  event.preventDefault(); //
+  console.log("cliclou botao")
+  console.log("digite o numero do cartão")
+  if (numInput.value === "") { //se minha variavel numInput tiver o valor identico a vazio retorne a mensagem  
+    return document.getElementById("resultado").innerHTML = "Digite o número do cartão."; //mensagem.
+  }
+  const valor = numInput.value; //variavel valor recebe o valor da variavel numInput 
+  if (numInput.value.length !== 16) {
+    return document.getElementById("resultado").innerHTML = "Cartão de Crédito precisa ter 16 dígitos."
+  }
+  console.log("só aceitar 16 caracteres")
+  if (isNaN(numInput.value)) {
+    return document.getElementById("resultado").innerHTML = "Caracteres invalidos, usar apenas números."
+  }
+  console.log("pegou o número")
+  if (validator.isValid(valor)) { // se a função isValid do objeto validator for verdadeiro execute bloco de código  
+    const maskedNumbers = validator.maskify(valor); //chama a função maskify do objeto validator e armazena o resultado na const maskedNumber
+    return document.getElementById("resultado").innerHTML = maskedNumbers + " é um cartão válido."; //se o valor que passou na função isValid for valido retorne a mensagem : variavel maskedNumber concatenado com string 
+  }
+  console.log("devolve valido ou não valido")
+  return document.getElementById("resultado").innerHTML = "Esse cartão não é válido, tente novamente."; //senão retorna mensagem não válido.
 })
 
-botao.addEventListener("click", function zerarInput(){
-    document.getElementById("userNum").value = ""; 
+
+botao.addEventListener("click", function zerarInput() { //zera o input quando o botao é clicado 
+  document.getElementById("meuInput").value = ""; // pega o valor do elemento input e faz ele ficar vazio? 
 })
-*/
+
+console.log(validator);
+
+
+/* numero cartão válido para test: 3379513561108795 */
